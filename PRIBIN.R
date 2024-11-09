aff32c16bfba97c47ace1afaa578f28d03e3b01c
PRIBINKBENNY <pb23abo@herts.ac.uk>
2024-11-09 20:34:39 +0000

    data loading and summary

--- PRIBIN.R
+++ PRIBIN.R
@@ -1,1 +1,13 @@
-print("Hello, World!")
+# Load necessary library
+library(readr)
+
+# Read the CSV file
+#salary_data <- read_csv("/Users/pribinkbenny/College_Works/salary_data.csv")
+salary_data <- read_csv("salary_data.csv")
+
+exists("salary_data")  # Returns TRUE if the object exists
+
+# View the first few rows of the data
+head(salary_data)
+str(salary_data)
+summary(salary_data)
