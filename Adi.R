f6b8b354f6c0184cc7bf9e2a82d5e77a200e4ede
adiz777 <az23abc@herts.ac.uk>
2024-11-24 00:27:44 +0000

    Deleted file

--- Adi.R
+++
@@ -1,4 +0,0 @@
-# Create a box plot of average_salary by continent_name
-boxplot(average_salary ~ continent_name, data = df,
-        xlab = "Continent", ylab = "Average Salary",
-        main = "Average Salary by Continent")
